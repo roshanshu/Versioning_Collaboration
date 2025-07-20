2420f615ce2ee4f2878354a176e995316a0f217c
Roshan Singh <roshanshu@gmail.com>
2025-07-20 21:06:27 +0530

    Hyperparameter tuning with MLFlow Child logging

--- src/filetune.py
+++ src/filetune.py
@@ -28,16 +28,15 @@ grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=6, n_jobs=-1,
 
 
 mlflow.set_experiment('NITP_FDP_3')
-#with mlflow.start_run() as parent:
-with mlflow.start_run():
+with mlflow.start_run() as parent:
     grid_search.fit(X_train, y_train)
 
         # log all the child runs
-        # for i in range(len(grid_search.cv_results_['params'])):
+    for i in range(len(grid_search.cv_results_['params'])):
 
-        #     with mlflow.start_run(nested=True) as child:
-        #         mlflow.log_params(grid_search.cv_results_["params"][i])
-        #         mlflow.log_metric("accuracy", grid_search.cv_results_["mean_test_score"][i])
+        with mlflow.start_run(nested=True) as child:
+            mlflow.log_params(grid_search.cv_results_["params"][i])
+            mlflow.log_metric("accuracy", grid_search.cv_results_["mean_test_score"][i])
 
         # Displaying the best parameters and the best score
     best_params = grid_search.best_params_
