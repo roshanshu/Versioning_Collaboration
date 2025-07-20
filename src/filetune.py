1869226880c9903cfdd9ac0d22fa32613564348f
Roshan Singh <roshanshu@gmail.com>
2025-07-20 21:01:25 +0530

    Hyperparameter tuning with MLFlow Logging

--- src/filetune.py
+++ src/filetune.py
@@ -29,48 +29,48 @@ grid_search = GridSearchCV(estimator=rf, param_grid=param_grid, cv=6, n_jobs=-1,
 
 mlflow.set_experiment('NITP_FDP_3')
 #with mlflow.start_run() as parent:
-with mlflow.start_run()
-grid_search.fit(X_train, y_train)
+with mlflow.start_run():
+    grid_search.fit(X_train, y_train)
 
-    # log all the child runs
-    # for i in range(len(grid_search.cv_results_['params'])):
+        # log all the child runs
+        # for i in range(len(grid_search.cv_results_['params'])):
 
-    #     with mlflow.start_run(nested=True) as child:
-    #         mlflow.log_params(grid_search.cv_results_["params"][i])
-    #         mlflow.log_metric("accuracy", grid_search.cv_results_["mean_test_score"][i])
+        #     with mlflow.start_run(nested=True) as child:
+        #         mlflow.log_params(grid_search.cv_results_["params"][i])
+        #         mlflow.log_metric("accuracy", grid_search.cv_results_["mean_test_score"][i])
 
-    # Displaying the best parameters and the best score
-best_params = grid_search.best_params_
-best_score = grid_search.best_score_
+        # Displaying the best parameters and the best score
+    best_params = grid_search.best_params_
+    best_score = grid_search.best_score_
 
-# Log params
-mlflow.log_params(best_params)
+    # Log params
+    mlflow.log_params(best_params)
 
-# Log metrics
-mlflow.log_metric("accuracy", best_score)
+    # Log metrics
+    mlflow.log_metric("accuracy", best_score)
 
-# Log training data
-train_df = X_train.copy()
-train_df['target'] = y_train
+    # Log training data
+    train_df = X_train.copy()
+    train_df['target'] = y_train
 
-train_df = mlflow.data.from_pandas(train_df)
-mlflow.log_input(train_df, "training")
+    train_df = mlflow.data.from_pandas(train_df)
+    mlflow.log_input(train_df, "training")
 
-# # Log test data
-test_df = X_test.copy()
-test_df['target'] = y_test
+    # # Log test data
+    test_df = X_test.copy()
+    test_df['target'] = y_test
 
-test_df = mlflow.data.from_pandas(test_df)
-mlflow.log_input(test_df, "testing")
+    test_df = mlflow.data.from_pandas(test_df)
+    mlflow.log_input(test_df, "testing")
 
-# Log source code
-mlflow.log_artifact(__file__)
+    # Log source code
+    mlflow.log_artifact(__file__)
 
-# Log the best model
-mlflow.sklearn.log_model(grid_search.best_estimator_, "random_forest")
+    # Log the best model
+    mlflow.sklearn.log_model(grid_search.best_estimator_, "random_forest")
 
-# Set tags
-mlflow.set_tag("author", "Roshan Singh")
+    # Set tags
+    mlflow.set_tag("author", "Roshan Singh")
 
-print(best_params)
-print(best_score)
+    print(best_params)
+    print(best_score)
